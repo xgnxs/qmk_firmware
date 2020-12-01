ef84fd65ac109bebdde2f7aaf6b47650ea307bec
Dylan Murphy <dylan_murphy@live.com>
2020-12-01 16:55:28 -0500

    planckify alt

--- keyboards/massdrop/alt/keymaps/xgnxs/keymap.c
+++ keyboards/massdrop/alt/keymaps/xgnxs/keymap.c
@@ -1,5 +1,12 @@
 #include QMK_KEYBOARD_H
 
+enum alt_layers {
+  _QWERTY,
+  _SYMBOL,
+  _NAV,
+  _ADJUST
+};
+
 enum alt_keycodes
 {
   U_T_AUTO = SAFE_RANGE, //USB Extra Port Toggle Auto Detect / Always Active
@@ -11,18 +18,13 @@ enum alt_keycodes
   MD_BOOT,               //Restart into bootloader after hold timeout
 };
 
-// tap for esc, hold for control
 #define CTL_ESC LCTL_T(KC_ESC)
-// tap for space, hold for layer 1
-#define SPC_FN LT(1, KC_SPC)
-// open windows terminal (really, whatever is pinned at 1)
-#define WIN_TRM G(KC_1) // G is an alias for LGUI
-// open windows terminal as admin (again, whatever is pinned at 1)
+#define SPC_FN LT(_SYMBOL, KC_SPC)
+#define WIN_TRM G(KC_1) 
 #define ADMIN_WT C(S(G(KC_1))) // Ctrl+Shift+Win+1
-// open windows clipboard
 #define WIN_CB G(KC_V)
-// alt F4 for closing windows
 #define ALT_F4 A(KC_F4)
+#define SCL_NAV LT(_NAV, KC_SCLN)
 
 keymap_config_t keymap_config;
 
@@ -35,19 +37,33 @@ keymap_config_t keymap_config;
 // add windows calc shortcut
 // make DEL FN be ALT+F4 instead of mute
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_65_ansi_blocker(
+    [_QWERTY] = LAYOUT_65_ansi_blocker(
         KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_DEL,  \
         KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_HOME, \
-        CTL_ESC,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,   WIN_TRM, \
+        CTL_ESC,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     SCL_NAV,  KC_QUOT,            KC_ENT,   WIN_TRM, \
         KC_LSFT,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    WIN_CB,  \
-        KC_LCTL,    KC_LGUI,  KC_LALT,                                SPC_FN,                                 KC_RALT,  MO(1),    KC_LEFT,  KC_DOWN,  KC_RGHT),\
+        KC_LCTL,    KC_LGUI,  KC_LALT,                                SPC_FN,                                 KC_RALT,  MO(_ADJUST),    KC_LEFT,  KC_DOWN,  KC_RGHT),\
 
-    [1] = LAYOUT_65_ansi_blocker(
+    [_SYMBOL] = LAYOUT_65_ansi_blocker(
         KC_ESC,     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  ALT_F4, \
-        _______,    RGB_SPD,  RGB_VAI,  RGB_SPI,  RGB_HUI,  RGB_SAI,  _______,  U_T_AUTO, U_T_AGCR, _______,  KC_PSCR,  KC_SLCK,  KC_PAUS,  _______,  KC_END,  \
-        KC_CAPS,    RGB_RMOD, RGB_VAD,  RGB_MOD,  RGB_HUD,  RGB_SAD,  KC_LPRN,  KC_LCBR,  KC_LBRC,  KC_EQL,   _______,  _______,            _______,  ADMIN_WT,\
-        _______,    RGB_TOG,  _______,  _______,  _______,  MD_BOOT,  NK_TOGG,  DBG_TOG,  _______,  _______,  _______,  _______,            KC_PGUP,  KC_CALC, \
+        _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_LPRN,  KC_RPRN,  _______,  _______,  _______,  KC_END,  \
+        KC_CAPS,    _______,  _______,  _______,  _______,  _______,  _______,  KC_LBRC,  KC_RBRC,  KC_MINS,  KC_EQL,   KC_BSLS,            _______,  ADMIN_WT,\
+        _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_PGUP,  KC_CALC, \
         _______,    _______,  _______,                                _______,                                _______,  _______,  KC_HOME,  KC_PGDN,  KC_END), \
+
+    [_NAV] = LAYOUT_65_ansi_blocker(
+        _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
+        _______,    _______,  _______,  _______,  _______,  KC_F5,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  _______,  _______,  _______,  _______,  \
+        _______,    _______,  _______,  KC_END,   KC_HOME,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,            _______,  _______,\
+        _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______, \
+        _______,    _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______), \
+    
+    [_ADJUST] = LAYOUT_65_ansi_blocker(
+        _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
+        _______,    RGB_SPD,  RGB_VAI,  RGB_SPI,  RGB_HUI,  RGB_SAI,  _______,  U_T_AUTO, U_T_AGCR, _______,  KC_PSCR,  KC_SLCK,  KC_PAUS,  _______,  _______,  \
+        _______,    RGB_RMOD, RGB_VAD,  RGB_MOD,  RGB_HUD,  RGB_SAD,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,\
+        _______,    RGB_TOG,  _______,  _______,  _______,  MD_BOOT,  NK_TOGG,  DBG_TOG,  _______,  _______,  _______,  _______,            _______,  _______, \
+        _______,    _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______) \
     /*
     [X] = LAYOUT(
         _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
