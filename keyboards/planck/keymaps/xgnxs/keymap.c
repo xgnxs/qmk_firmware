c7ca9954ab1a3a5ad462f7bf11f111c5bc66948f
Dylan Murphy <dylan_murphy@live.com>
2021-05-17 17:23:28 -0400

    update planck keymap, make first space shift

--- keyboards/planck/keymaps/xgnxs/keymap.c
+++ keyboards/planck/keymaps/xgnxs/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_DEL,
     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    SCL_NAV, CTL_QOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT,
-    KC_LCTL, BACKLIT, KC_RALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LALT, KC_RGUI, WIN_TRM, KC_RSFT
+    KC_LCTL, BACKLIT, KC_RALT, KC_LGUI, LOWER,   KC_LSFT, KC_SPC,  RAISE,   KC_LALT, KC_RGUI, WIN_TRM, KC_RSFT
 ),
 
 /* Lower
