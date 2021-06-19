e7fa40305ef1f7e04f6f232012695cf801535fb4
Dylan Murphy <dylan_murphy@live.com>
2021-06-19 15:13:50 -0400

    update with changes from master

--- keyboards/crkbd/keymaps/xgnxs/keymap.c
+++ keyboards/crkbd/keymaps/xgnxs/keymap.c
@@ -17,6 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
+#include <stdio.h>
 
 enum layer_names {
   _QWERTY,
@@ -142,7 +143,7 @@ bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
 
 #ifdef OLED_DRIVER_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_master) {
+  if (!is_keyboard_master()) {
     return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
   }
   return rotation;
@@ -228,7 +229,7 @@ void oled_render_logo(void) {
 }
 
 void oled_task_user(void) {
-    if (is_master) {
+    if (is_keyboard_master()) {
         oled_render_layer_state();
         oled_render_keylog();
     } else {
